ace5aca399bee835513d913483ee6e07a0463c05
Devon <Dvowen@cox.net>
2021-01-07 14:15:52 -0700

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# PossiblePortfolioChange
-Portfolio idea.
--- client/src/Components/CenterBox.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function CenterBox(props) {
-    return (
-        <div>
-            This is a test
-        </div>
-    )
-}
